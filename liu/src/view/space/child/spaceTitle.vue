731580066ede9a5edd8e5b66278791ba0a1ab9d9
liuao <aoyy0131@qq.com>
2018-12-27 16:41:15 +0800

    updata master

---
+++ liu/src/view/space/child/spaceTitle.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="spaceTitleSum">
+        <div class="spaceTitle" v-for="(item, index) in count" :key="index">
+            <AddSelect></AddSelect>            
+        </div>
+        <el-row>
+            <el-button @click="addSpace" class="addSpace">添加空间</el-button>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import AddSelect from './addSelect'
+
+export default {
+    data() {
+        return {
+            options: [
+                {
+                    value: '选项1',
+                    label: '黄金糕'
+                },
+                {
+                    value: '选项2',
+                    label: '双皮奶'
+                }
+            ],
+            value4: '',
+            count : 0,
+            //设备添加
+            list : [
+                {
+                    id : 0,
+                    count : 0
+                },
+                {
+                    id : 1,
+                    count : 1
+                }
+            ]
+        }
+    },
+    computed: {
+        
+    },
+    components: {
+        AddSelect
+    },
+    mounted() {
+        
+    },
+    methods: {
+        addSpace (index) {
+            this.count = this.$store.state.count;
+            this.count++;
+            this.$store.commit('get_spaceCount', this.count)
+        }
+    },
+}
+</script>
+
+<style>
+    .spaceTitleSum{
+        
+    }
+    .el-input{
+        border:1px solid #000;
+    }
+    .el-input__inner{
+        border:none;
+        color:#000;
+    }
+    .addClick{
+        margin-top:20px;
+        background: rgb(156, 123, 33);
+        color:#fff;
+    }
+    .spaceTitle{
+        border-bottom: 3px solid #000;
+        padding-bottom: 20px;
+        margin-bottom: 20px;
+    }
+    .addClick{
+        margin-top:20px;
+        background: rgb(97, 81, 8);
+        color:#fff;
+    }
+    .addKong{
+        display: inline-block;
+        width: 100px;
+    }
+    .addKong input{
+        font-size:12px;
+        background: rgb(97, 81, 8);
+        color:#fff;
+    }
+    .spaceTitle-input{
+        width: 130px;
+    }
+    .addSelect-input{
+        width: 130px;
+    }
+    .spaceTitle-cen, .addSelect{
+        margin-top:20px;
+    }
+    .dilite{
+        display: inline-block;
+        width: 30px;
+        height: 30px;
+        border-radius:50%;
+        text-align: center;
+        line-height: 30px;
+        color:#fff;
+        margin-left:10px;
+        background: rgb(180, 139, 5);
+    }
+</style>
---
+++ liu/src/view/space/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="space">
+        <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+        <el-dialog
+            title="编辑设备信息"
+            :visible.sync="dialogVisible"
+            width="70%"
+            :before-close="handleClose">
+            <!-- 内容组件 -->
+            <SpaceTitle></SpaceTitle>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+import SpaceTitle from './child/spaceTitle'
+
+export default {
+    data() {
+        return {
+            dialogVisible: false
+        };
+    },
+    components: {
+        SpaceTitle
+    },
+    methods: {
+        handleClose(done) {
+            this.$confirm('确认关闭？')
+            .then(_ => {
+                done();
+            })
+            .catch(_ => {});
+        }
+    }
+};
+</script>
+
+<style>
+    .space{
+        width: 100%;
+        height: 100%;
+    }
+    .el-dialog__wrapper{
+        width: 100%;
+        height: 100%;
+    }
+    .el-dialog{
+        width: 70%;
+        height: 70%;
+    }
+    .el-dialog__body{
+        padding: 20px 20px 10px;
+    }
+    .el-dialog{
+        width: 100%;
+        height: 100%;
+        overflow-y: auto;
+    }
+    .el-dialog__footer{
+        text-align: center;
+    }
+    .el-button--default{
+        width: 100px;
+        margin-right: 5px;
+        border:1px solid rgb(119, 77, 8);
+    }
+    .el-button--primary{
+        width: 100px;
+        margin-left: 5px;
+        border:1px solid rgb(119, 77, 8);
+        background: rgb(119, 77, 8);
+    }
+</style>
